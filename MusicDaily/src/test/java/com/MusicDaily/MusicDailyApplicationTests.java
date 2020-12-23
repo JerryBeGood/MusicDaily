9890709369c7a5c5336f8454854be4d06bb80e33
Marcin Dobrowolski <mdobrowolski.1997@gmail.com>
2020-12-23 12:10:13 +0100

    Initial code

---
+++ MusicDaily/src/test/java/com/MusicDaily/MusicDailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MusicDaily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicDailyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
